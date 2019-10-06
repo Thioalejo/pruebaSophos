9ddad3685fb6770f8044462e8c0a910d1beda4b8
Thioalejo <alejo.saraza@gmail.com>
2019-10-06 09:49:47 -0500

    Creacion de proyecto base

---
+++ src/test/java/co/com/sophos/pruebasophos/DefinitionTestSuite.java
@@ -0,0 +1,9 @@
+package co.com.sophos.pruebasophos;
+
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features="src/test/resources/features")
+public class DefinitionTestSuite {}
